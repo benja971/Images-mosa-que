1a200258bace5ffa5729a217e0f875d11ee39b39
benja971 <46868460+benja971@users.noreply.github.com>
2020-12-04 17:03:11 -0400

    tests images couleur

--- Barack.py
+++ Barack.py
@@ -2,42 +2,67 @@ from os import listdir, system
 from PIL import Image as im, ImageDraw as imdrw
 from random import randint, choice
 
+
+
+def diffTuples(t1, i, j)->tuple:
+	c = [t1[0], t1[0], t1[0]]
+	c[j] = t1[j] - i
+
+	return (c[0], c[1], c[2])
+
+def dist(t1, t2):
+	c = []
+
+	c.append(abs(t1[0] - t2[0]))
+	c.append(abs(t1[1] - t2[1]))
+	c.append(abs(t1[2] - t2[2]))
+	
+	return (c[0], c[1], c[2])
+
 system("cls")
 
-PATH = 'Images'
+PATH = "D:/Drive/test"
 SCALE = 50 # Taille des carrés
 DIVS = 150 # Nombre d'images en hauteurs
 
 if __name__ == "__main__":
-	files = listdir(f'Drive/{PATH}/')
-	print(f'Loading {len(files)} images from Drive/{PATH}/...')
+	files = listdir(f"{PATH}")
+	print(f"{len(files)} images loaded from {PATH}/...")
+	# files2 = [files[n] for n in range(len(files)//2)]
 	images = {}
-	
+	c = 0
 	for file in files:
-		if file:
-			imgb = im.open(f"Drive/{PATH}/{file}")
-			imgr =  im.new('RGB', (SCALE, SCALE))
-			cW, cH = imgb.size
-			moy = 0
-			if cW >= SCALE and cH >= SCALE:
-				for x in range(SCALE):
-					for y in range(SCALE):
-						size = min(cH, cW)
-						coords = int(x/SCALE * size), int(y/SCALE*size)
-						cc = sum(imgb.getpixel(coords))//3
-						imgr.putpixel((x, y), (cc, cc, cc))
-						moy += cc
-		if not moy//SCALE**2 in images:
-		 	images[moy//SCALE**2] = []
-		images[moy//SCALE**2].append(imgr)
+		print(f"{int(c/len(files)*100)}% of the image(s) processed", end="\r")
+		c+=1
+		imgb = im.open(f"{PATH}/{file}")
+		imgr =  im.new("RGB", (SCALE, SCALE))
+		cW, cH = imgb.size
+		R, G, B = 0, 0, 0
 
-	print(f'Scaled {len(images)} images from: Drive/{PATH}/{file}...')
+		for x in range(SCALE):
+			for y in range(SCALE):
+				size = min(cH, cW)
+				coords = int(x/SCALE * size), int(y/SCALE*size)
+				color = imgb.getpixel(coords)
+				imgr.putpixel((x, y), color)
+				R += color[0]
+				G += color[1]
+				B += color[2]
 
-	ref = im.open(f'Drive/{PATH}/Chat.jpeg')
-	print(f"Drive/{PATH}/Chat.jpeg loaded")
+		R//=SCALE**2
+		G//=SCALE**2
+		B//=SCALE**2
+
+		if (R, G, B) not in images:
+			images[(R, G, B)] = []
+		images[(R, G, B)].append(imgr)
+		
+
+	ref = im.open(f"{PATH}/IMG_20201002_181635_480.jpg")
+	print(f"{PATH}/IMG_20201002_181635_480.jpg loaded")
 	refW, refH = ref.size
 	W, H = DIVS * refW // refH, DIVS
-	res = im.new('RGB', (W * SCALE, H * SCALE))
+	res = im.new("RGB", (W * SCALE, H * SCALE))
 
 	ctx = imdrw.Draw(res)
 	for x in range(W):
@@ -48,11 +73,13 @@ if __name__ == "__main__":
 			y1 = y * SCALE 
 			x2 = (x+1) * SCALE 
 			y2 = (y+1) * SCALE
-			c = sum(c)//3
-			# ctx.rectangle([x1, y1, x2, y2], fill = (c, c, c), width=0)
-			if c in images:
-				res.paste(choice(images[c]), (x1, y1))
+			for j in range(3):
+				for i in range(-120, 120):
+					if dist(c, diffTuples(c, i, j)) <= (1, 1, 1):
+						if diffTuples(c, i, j) in images:
+							res.paste(choice((images[diffTuples(c, i, j)])), (x1, y1))
 
-print(f'Creating {PATH}.png...')
-res.save(PATH + '.png')
-system(PATH + '.png')
+print(f"Creating {PATH}.png...")
+res.save(PATH + ".png")
+print(f"Opening {PATH}.png...")
+system(PATH + ".png")
